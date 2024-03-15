e02f32d9f76695656e38119a158b29cb53f90417
jbetinpantoja <jbetin@blossom.net>
2024-03-15 12:11:42 -0500

    connect and configure prisma with model user

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,4 @@
+import 'reflect-metadata';
 import express, { Application } from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
---
+++ src/prisma/migrations/20240315164942_init/migration.sql
@@ -0,0 +1,13 @@
+-- CreateTable
+CREATE TABLE "users" (
+    "id" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "users_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "users_email_key" ON "users"("email");
